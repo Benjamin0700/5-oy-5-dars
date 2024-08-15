e6b9e6fb021f6ad777c750f9eb7a582d24df48c3
Benjamin <jalolsoxiddinov777@gmail.com>
2024-08-15 18:17:53 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -11,19 +11,30 @@
 // document.body.style.backgroundColor = "black";
 
 
+// let h1 = document.querySelector(".h1");
+
+// h1.textContent = `Benjamin`;
+
+
+// let ism = prompt(`Ism kiriting:`)
+// let rang = prompt(`Rang kiriting:`)
+
+// h1.textContent = `Benjamin`;
+// h1.innerHTML = `<mark>${ism}</mark>`;
+// h1.style.textAlign = "Center";
+// h1.style.backgroundColor = `${rang}`;
+
+// let img = document.querySelector(".img");
+// img.setAttribute("src", "https://images.uzum.uz/cikn0tb6edfostiikm70/original.jpg")
+// img.style.width = "50px"
+
+
+
 let h1 = document.querySelector(".h1");
-
-h1.textContent = `Benjamin`;
-
-
 let ism = prompt(`Ism kiriting:`)
-let rang = prompt(`Rang kiriting:`)
 
-h1.textContent = `Benjamin`;
 h1.innerHTML = `<mark>${ism}</mark>`;
-h1.style.textAlign = "Center";
-h1.style.backgroundColor = `${rang}`;
-
-let img = document.querySelector(".img");
-img.setAttribute("src", "https://images.uzum.uz/cikn0tb6edfostiikm70/original.jpg")
-img.style.width = "50px"
+h1.style.fontSize = "200px";
+h1.style.textAlign = "center";
+h1.style.backgroundColor = "blue";
+document.body.style.backgroundColor = "blue";
